e5951a9bb1e184edcf3db8ac77a9ca639f196dce
Nick Golding <goldingn35@gmail.com>
2017-07-13 13:34:30 +1000

    document

---
+++ R/package.R
@@ -0,0 +1,12 @@
+#' gretaGAM: generalised Additive Models in greta using mgcv
+#' @name gretaGAM
+#'
+#' @description gretaGAM is a module for greta that lets you use mgcv's smoother
+#'   functions and formula syntax to define smooth terms for use in a greta
+#'   model. You can then define your own likelihood to complete the model, and
+#'   fit it by MCMC.
+#'
+#' @docType package
+#' @import greta
+#' @import mgcv
+NULL
