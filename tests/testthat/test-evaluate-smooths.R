757795dad6cbc9c54da4d87d6dd4e0038b6e8a08
njtierney <nicholas.tierney@gmail.com>
2024-12-11 17:46:34 +1100

    make sure to skip greta related tests on CRAN

--- tests/testthat/test-evaluate-smooths.R
+++ tests/testthat/test-evaluate-smooths.R
@@ -1,16 +1,18 @@
-n <- 30
-x <- runif(n, 0, 10)
-f <- function(x) {
-  sin(x * 2) + 1.6 * (x < 3) - 1.4 * (x > 7)
-}
-y <- f(x) + rnorm(n, 0, 0.3)
-x_plot <- seq(0, 10, length.out = 200)
-
-z <- smooths(~ s(x), data = data.frame(x = x))
-
-distribution(y) <- normal(z, 0.3)
-
 test_that("evaluate_smooths errors appropriately", {
+  skip_if_not(check_tf_version())
+  n <- 30
+  x <- runif(n, 0, 10)
+  f <- function(x) {
+    sin(x * 2) + 1.6 * (x < 3) - 1.4 * (x > 7)
+  }
+  y <- f(x) + rnorm(n, 0, 0.3)
+  x_plot <- seq(0, 10, length.out = 200)
+
+  z <- smooths(~ s(x), data = data.frame(x = x))
+
+  distribution(y) <- normal(z, 0.3)
+
+
   expect_snapshot(
     error = TRUE,
     evaluate_smooths("thing")
--- tests/testthat/test-stop-when-dummy-in-data.R
+++ tests/testthat/test-stop-when-dummy-in-data.R
@@ -1,20 +1,21 @@
-test_df <- data.frame(
-  dummy = 1:5,
-  thing = LETTERS[1:5]
-)
-
-n <- 30
-x <- runif(n, 0, 10)
-f <- function(x) {
-  sin(x * 2) + 1.6 * (x < 3) - 1.4 * (x > 7)
-}
-y <- f(x) + rnorm(n, 0, 0.3)
-x_plot <- seq(0, 10, length.out = 200)
-
-z <- smooths(~ s(x), data = data.frame(x = x))
-
-
 test_that("stop_when_dummy_in_data errors appropriately", {
+  skip_if_not(check_tf_version())
+
+  test_df <- data.frame(
+    dummy = 1:5,
+    thing = LETTERS[1:5]
+  )
+
+  n <- 30
+  x <- runif(n, 0, 10)
+  f <- function(x) {
+    sin(x * 2) + 1.6 * (x < 3) - 1.4 * (x > 7)
+  }
+  y <- f(x) + rnorm(n, 0, 0.3)
+  x_plot <- seq(0, 10, length.out = 200)
+
+  z <- smooths(~ s(x), data = data.frame(x = x))
+
   expect_snapshot(
     error = TRUE,
     stop_when_dummy_in_data(test_df)
